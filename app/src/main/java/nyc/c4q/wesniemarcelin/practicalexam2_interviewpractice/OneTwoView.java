c1fab357e26788459f1db108bec35caabdd8767c
wesniem <wesniem@yahoo.com>
2016-12-21 21:40:38 -0500

    Began Custom View Activity

---
+++ app/src/main/java/nyc/c4q/wesniemarcelin/practicalexam2_interviewpractice/OneTwoView.java
@@ -0,0 +1,15 @@
+package nyc.c4q.wesniemarcelin.practicalexam2_interviewpractice;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by wesniemarcelin on 12/21/16.
+ */
+
+public class OneTwoView extends View {
+    public OneTwoView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
